51afb4194e26deb92456b047aff93c14ecdc6dd9
Ace-SML <ace-sml@users.noreply.github.com>
2026-02-18 21:52:21 +0100

    Updated the bg color

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import MusicSection from './components/MusicSection';
-import Gallery from './components/Gallery';
-import TourSection from './components/TourSection';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import MusicSection from "./components/MusicSection";
+import Gallery from "./components/Gallery";
+import TourSection from "./components/TourSection";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black text-white selection:bg-blue-600 selection:text-white">
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#ff3300a9",
+        backgroundSize: "cover",
+        color: "#ffffff",
+      }}
+    >
       <Header />
       <main>
         <Hero />
